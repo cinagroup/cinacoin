c34d8314bbc7bc965ac0b81ecd53f56dcb022d78
CinaGroup <dev@cinagroup.io>
2026-06-07 14:01:36 +0000

    chore: add docusaurus build to .gitignore, remove from repo

--- apps/docs-site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/cina/.openclaw/workspace/onux/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/home/cina/.openclaw/workspace/onux/node_modules/.pnpm/@docusaurus+theme-classic@3.10.1_@types+react@18.3.12_bufferutil@4.1.0_esbuild@0.25.12_react-_dcjahyyxbngo5w4rawqufsqrly/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/cina/.openclaw/workspace/onux/node_modules/.pnpm/@docusaurus+theme-classic@3.10.1_@types+react@18.3.12_bufferutil@4.1.0_esbuild@0.25.12_react-_dcjahyyxbngo5w4rawqufsqrly/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/cina/.openclaw/workspace/onux/apps/docs-site/src/css/custom.css"),
-];
--- apps/docs-site/build/assets/js/36994c47.e5870941.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkcinacoin_docs=globalThis.webpackChunkcinacoin_docs||[]).push([[9858],{5516(c){c.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
